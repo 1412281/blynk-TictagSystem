4c594a4aa8fa3ac1cb4a7490278c73d2268cd510
anhthang23597 <anhthang23597@gmail.com>
2018-08-24 16:18:17 +0700

    add hc

--- Room/Room.ino
+++ Room/Room.ino
@@ -38,18 +38,229 @@
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
 
+//chân ST_CP (Chân 12) của 74HC595
+int latchPin1 = 4;
+int latchPin2 = 5;
+int latchPin3 = 6;
+//chân SH_CP (Chân 11) của 74HC595
+int clockPin = 3;
+//Chân DS (chân 14) của 74HC595
+int dataPin = 2;
+//Trạng thái của LED, hay chính là byte mà ta sẽ gửi qua shiftOut
+
 void           (*resetFunc)(void) = 0; //declare reset function at address 0
 
 byte           tokenHasSet = 0;
 
 char TOKEN_DEFAULT[] = "0f1add3742174f88bc8f93f37486c3b9";
+char buffer[50];
+byte ledStatus1 = 0b00000000;
+byte ledStatus2 = 0b00000000;
+byte ledStatus3 = 0b00000000;
+
+int relay;
+int state1;
+byte state2;
+int state[25]={0,0b00000001,0b00000010,0b00000100,0b00001000,0b00010000,0b00100000,0b01000000,0b10000000,0b00000001,0b00000010,0b00000100,0b00001000,0b00010000,0b00100000,0b01000000,0b10000000,0b00000001,0b00000010,0b00000100,0b00001000,0b00010000,0b00100000,0b01000000,0b10000000};
+//...............................................
 
 BLYNK_WRITE(V0)
 {
     Serial.println("WebHook data:");
     Serial.println(param.asStr());
 }
-
+BLYNK_WRITE(V1)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 1 ;
+    Relay_Control(relay,state1);
+}
+BLYNK_WRITE(V2)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 2 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V3)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 3 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V4)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 4 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V5)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 5 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V6)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 6 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V7)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 7 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V8)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 8 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V9)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 9 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V10)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 10 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V11)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 11 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V12)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 12 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V13)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 13 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V14)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 14 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V15)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 15 ;
+    Relay_Control(relay,state1);  
+}BLYNK_WRITE(V16)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 16 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V17)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 17 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V18)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 18 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V19)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 19 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V20)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 20 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V21)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 21 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V22)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 22 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V23)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 23 ;
+    Relay_Control(relay,state1);  
+}
+BLYNK_WRITE(V24)
+{
+//    Serial.println("WebHook data:");
+//    Serial.println(param.asInt());
+    int state1 = param.asInt();
+    relay = 24 ;
+    Relay_Control(relay,state1);  
+}
+//..............................
 BLYNK_WRITE(V31)
 {
     Serial.print("New token:");
@@ -82,6 +293,13 @@ BLYNK_WRITE(V31)
 void setup()
 {
     // Debug console
+    //Bạn BUỘC PHẢI pinMode các chân này là OUTPUT
+    pinMode(latchPin1, OUTPUT);
+    pinMode(latchPin2, OUTPUT);
+    pinMode(latchPin3, OUTPUT);
+    pinMode(clockPin, OUTPUT);
+    pinMode(dataPin, OUTPUT);
+    
     Serial.begin(9600);
 
     tokenHasSet = EEPROM.read(TOKEN_HAS_SET_ADDRESS);
@@ -111,7 +329,74 @@ void setup()
 
     //Blynk.begin(auth, IPAddress(192,168,1,100), 8080);
 }
-
+//...............................................
+void Relay_Control (int relay,int state1)
+{
+  if(relay<=8)
+  {
+  state2 = bitRead(ledStatus1,relay - 1);
+  Serial.print(state1);
+  Serial.println(state2);
+  if(state2 == state1)
+  {
+  Serial.print(relay);
+  Serial.println(state1);
+  }
+  else
+  {
+  ledStatus1 ^= state[relay];
+  digitalWrite(latchPin1, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, ledStatus1);  
+  digitalWrite(latchPin1, HIGH);//các đèn LED sẽ sáng với trạng thái vừa được cập nhật
+  Serial.print(relay);
+  Serial.println(state1);
+  Serial.println("TH1");
+  }
+  }
+  else if(relay>=9 and relay<=16)
+  {
+  state2 = bitRead(ledStatus2,relay - 9);
+  Serial.print(state1);
+  Serial.println(state2);
+  if(state2 == state1)
+  {
+  Serial.print(relay);
+  Serial.println(state1);
+  }
+  else
+  {
+  ledStatus2 ^= state[relay];
+  digitalWrite(latchPin2, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, ledStatus2);  
+  digitalWrite(latchPin2, HIGH);//các đèn LED sẽ sáng với trạng thái vừa được cập nhật 
+  Serial.print(relay);
+  Serial.println(state1);
+  Serial.println("TH2");
+  }
+  }
+  else if(relay>=17 and relay<=24)
+  {
+  state2 = bitRead(ledStatus3,relay - 17);
+  Serial.print(state1);
+  Serial.println(state2);
+  if(state2 == state1)
+  {
+  Serial.print(relay);
+  Serial.println(state1);    
+  }
+  else
+  {  
+  ledStatus3 ^= state[relay];
+  digitalWrite(latchPin3, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, ledStatus3);  
+  digitalWrite(latchPin3, HIGH);//các đèn LED sẽ sáng với trạng thái vừa được cập nhật 
+  Serial.print(relay);
+  Serial.println(state1);
+  Serial.println("TH3"); 
+  }
+  }
+}
+//................................................................
 void loop()
 {
     Blynk.run();
